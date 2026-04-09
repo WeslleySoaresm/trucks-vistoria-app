70e56f7c6317a548f5857d724e1917c8d2ae6ab0
weslleysoares <correweslleysoares@gmail.com>
2026-04-09 08:12:13 +0100

    feat: update package.json and vite.config.js to include gh-pages and set base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Isso faz com que os caminhos sejam relativos e funcionem em qualquer lugar
 })
